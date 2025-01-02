48e225418f888b3715eabdf0725e9fd0cf3e9cd3
saksham1520 <saksham1520@gmail.com>
2025-01-02 18:44:34 +0530

    changes url

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: "http://localhost:3000", // Adjust the allowed origin as needed
+    origin: "https://restaurantbooking11.netlify.app",
 };
 
 app.use(cors(corsOptions));
 
-// Booking Routes
+
 app.use("/api", bookingRoute);
 
-// Vercel's serverless function export
+
 export default function handler(req, res) {
-    app(req, res);  // This makes the Express app handle requests for serverless functions
+    app(req, res);
 }
 
-// Ensure DB connection and server startup (you can remove this if you want Vercel to handle the serverless scaling)
+
 connectDB();
 console.log("Vercel Serverless API is ready!");
